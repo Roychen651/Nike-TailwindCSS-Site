c18e1c24ac8189dda181c39a1adf44ad1ae0a073
Roy Chen <roy97chen@gmail.com>
2024-08-01 20:55:25 +0300

    services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {services} from '../constants'
+import ServiceCard from '../Components/ServiceCard'
 
 const Services = () => {
   return (
-    <div>
-        
-    </div>
+    <section className='max-container flex justify-center flex-wrap
+    gap-9'>
+      {services.map((service)=>(
+        <ServiceCard key={service.label}
+        {...service}/>
+      ))}
+
+
+    </section>
   )
 }
 
